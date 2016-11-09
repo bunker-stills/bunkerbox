6522ce33ec914e0aaa015b5a36060d50f1702598
Jim Heising <jheising@gmail.com>
2016-11-09 14:31:37 -0800

    .

---
+++ services/bunkerbox_server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host_run/dbus/system_bus_socket \
+  dbus-send \
+  --system \
+  --print-reply \
+  --reply-timeout=2000 \
+  --type=method_call \
+  --dest=org.freedesktop.hostname1 \
+  /org/freedesktop/hostname1 \
+  org.freedesktop.hostname1.SetStaticHostname \
+  string:$DEVICE_ID boolean:true
