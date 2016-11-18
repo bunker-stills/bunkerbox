b3219199b14b8d85cc7c346b58e20e0397f66321
Jim Heising <jheising@gmail.com>
2016-11-18 14:02:43 -0800

    Updated PID

--- processes/lib/pid.js
+++ processes/lib/pid.js
@@ -1,280 +1,104 @@
-/**
- * pid-controller -  A node advanced PID controller based on the Arduino PID library
- * github@wilberforce.co.nz Rhys Williams
- * Based on:
- * Arduino PID Library - Version 1.0.1
- * by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
- *
- * This Library is licensed under a GPL-3.0 License
- */
+var _ = require("underscore");
 
-"use strict";
-var PID = function(Input, Setpoint, Kp, Ki, Kd, ControllerDirection) {
-    this.input = Input;
-    this.mySetpoint = Setpoint;
-    this.inAuto = false;
-    this.setOutputLimits(0, 255); // default output limits
-    this.SampleTime = 100; // default Controller Sample Time is 0.1 seconds
-    this.setTunings(Kp, Ki, Kd);
-    this.setControllerDirection(ControllerDirection);
-    this.lastTime = this.millis() - this.SampleTime;
+function pid()
+{
+    this.PV = 0;
+    this.CV = 0;
+    this.Kp = 0;
+    this.Ki = 0;
+    this.Kd = 0;
+    this.CVLowerLimit = 0;
+    this.reset();
+}
+module.exports = pid;
 
-    this.ITerm = 0;
-    this.myOutput = 0;
-};
+pid.prototype.reset = function()
+{
+    this.lastMeasurementTime = undefined;
+    this.setPoint = 0;
+    this.previousError = 0;
+    this.integral = 0;
+}
 
-// Constants for backward compatibility
-PID.AUTOMATIC = 1;
-PID.MANUAL = 0;
-PID.DIRECT = 0;
-PID.REVERSE = 1;
+pid.prototype.setDesiredValue = function(setPoint)
+{
+    this.setPoint = setPoint;
+}
 
-PID.prototype.setInput = function(current_value) {
-    this.input = current_value;
-};
+pid.prototype.setProportionalGain = function(Kp)
+{
+    this.Kp = Kp;
+}
 
-PID.prototype.setPoint = function(current_value) {
-    this.mySetpoint = current_value;
-};
+pid.prototype.setIntegralGain = function(Ki)
+{
+    this.Ki = Ki;
+}
 
-PID.prototype.millis = function() {
-    var d = new Date();
-    return d.getTime();
-};
+pid.prototype.setDerivativeGain = function(Kd)
+{
+    this.Kd = Kd;
+}
 
-/**
- * Compute()
- * This, as they say, is where the magic happens.  this function should be called
- * every time "void loop()" executes.  the function will decide for itself whether a new
- * pid Output needs to be computed.  returns true when the output is computed,
- * false when nothing has been done.
- */
-PID.prototype.compute = function() {
-    if (!this.inAuto) {
-        return false;
-    }
-    var now = this.millis();
-    var timeChange = (now - this.lastTime);
-    if (timeChange >= this.SampleTime) {
+pid.prototype.update = function(measuredValue)
+{
+    var now = Date.now();
 
-        // Compute all the working error variables
-        var input = this.input;
-        var error = this.mySetpoint - input;
-        this.ITerm += (this.ki * error);
-        var dInput = input - this.lastInput;
-        // Compute PID Output
-        var output = (this.kp * error + this.ITerm - this.kd * dInput) * this.setDirection;
-
-        if (output > this.outMax) {
-            this.ITerm -= (output - this.outMax);
-            output = this.outMax;
-        }
-        else if (output < this.outMin) {
-            this.ITerm += (this.outMin - output);
-            output = this.outMin;
-        }
-        this.myOutput = output;
-
-        // Remember some variables for next time
-        this.lastInput = input;
-        this.lastTime = now;
-        return true;
-    }
-    else {
-        return false;
-    }
-};
-
-/**
- * SetTunings(...)
- * This function allows the controller's dynamic performance to be adjusted.
- * it's called automatically from the constructor, but tunings can also
- * be adjusted on the fly during normal operation
- */
-PID.prototype.setTunings = function(Kp, Ki, Kd) {
-    if (Kp < 0 || Ki < 0 || Kd < 0) {
-        return;
+    if(_.isUndefined(this.lastMeasurementTime))
+    {
+        this.lastMeasurementTime = now;
+        return this.CVLowerLimit;
     }
 
-    this.dispKp = Kp;
-    this.dispKi = Ki;
-    this.dispKd = Kd;
+    var dt = (now - this.lastMeasurementTime) / 1000.0;
+    var input = measuredValue
+    var integral = this.integral;
 
-    this.SampleTimeInSec = (this.SampleTime) / 1000;
-    this.kp = Kp;
-    this.ki = Ki * this.SampleTimeInSec;
-    this.kd = Kd / this.SampleTimeInSec;
-};
+    var error = this.setPoint - input;
 
-/**
- * SetSampleTime(...)
- * sets the period, in Milliseconds, at which the calculation is performed
- */
-PID.prototype.setSampleTime = function(NewSampleTime) {
-    if (NewSampleTime > 0) {
-        var ratio = NewSampleTime / (1.0 * this.SampleTime);
-        this.ki *= ratio;
-        this.kd /= ratio;
-        this.SampleTime = Math.round(NewSampleTime);
-    }
-};
+    integral = integral + (this.Ki * error * dt);
 
-/**
- * SetOutput( )
- * Set output level if in manual mode
- */
-PID.prototype.setOutput = function(val) {
-    if (val > this.outMax) {
-        this.myOutput = val;
-    }
-    else if (val < this.outMin) {
-        val = this.outMin;
-    }
-    this.myOutput = val;
-};
+    var derivative = (error - this.previousError) / dt;
 
-/**
- * SetOutputLimits(...)
- * This function will be used far more often than SetInputLimits.  while
- * the input to the controller will generally be in the 0-1023 range (which is
- * the default already,)  the output will be a little different.  maybe they'll
- * be doing a time window and will need 0-8000 or something.  or maybe they'll
- * want to clamp it from 0-125.  who knows.  at any rate, that can all be done here.
- */
-PID.prototype.setOutputLimits = function(Min, Max) {
-    if (Min >= Max) {
-        return;
-    }
-    this.outMin = Min;
-    this.outMax = Max;
+    var CV = this.Kp * error + integral + this.Kd * derivative;
 
-    if (this.inAuto) {
-        if (this.myOutput > this.outMax) {
-            this.myOutput = this.outMax;
-        }
-        else if (this.myOutput < this.outMin) {
-            this.myOutput = this.outMin;
-        }
-
-        if (this.ITerm > this.outMax) {
-            this.ITerm = this.outMax;
-        }
-        else if (this.ITerm < this.outMin) {
-            this.ITerm = this.outMin;
-        }
-    }
-};
-
-/**
- * SetMode(...)
- * Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
- * when the transition from manual to auto occurs, the controller is
- * automatically initialized
- */
-PID.prototype.setMode = function(Mode) {
-    var newAuto;
-    if (Mode == 0 || Mode.toString().toLowerCase() == 'automatic' || Mode.toString().toLowerCase() == 'auto') {
-        newAuto = 1;
-    }
-    else if (Mode == 1 || Mode.toString().toLowerCase() == 'manual') {
-        newAuto = 0;
-    }
-    else {
-        throw new Error("Incorrect Mode Chosen");
+    if(!_.isUndefined(this.CVOffset))
+    {
+        CV += this.CVOffset;
     }
 
-    if (newAuto == !this.inAuto) { //we just went from manual to auto
-        this.initialize();
+    if(!_.isUndefined(this.CVUpperLimit) && CV > this.CVUpperLimit)
+    {
+        integral = this.integral;
+        CV = this.CVUpperLimit;
     }
-    this.inAuto = newAuto;
-};
 
-/**
- * Initialize()
- * does all the things that need to happen to ensure a bumpless transfer
- * from manual to automatic mode.
- */
-PID.prototype.initialize = function() {
-    this.ITerm = this.myOutput;
-    this.lastInput = this.input;
-    if (this.ITerm > this.outMax) {
-        this.ITerm = this.outMax;
+    if(!_.isUndefined(this.CVLowerLimit) && CV < this.CVLowerLimit)
+    {
+        integral = this.integral;
+        CV = this.CVLowerLimit;
     }
-    else if (this.ITerm < this.outMin) {
-        this.ITerm = this.outMin;
-    }
-};
 
-/**
- * SetControllerDirection(...)
- * The PID will either be connected to a DIRECT acting process (+Output leads
- * to +Input) or a REVERSE acting process(+Output leads to -Input.)  we need to
- * know which one, because otherwise we may increase the output when we should
- * be decreasing.  This is called from the constructor.
- */
-PID.prototype.setControllerDirection = function(ControllerDirection) {
-    if (ControllerDirection == 0 || ControllerDirection.toString().toLowerCase() == 'direct') {
-        this.setDirection = 1;
-    }
-    else if (ControllerDirection == 1 || ControllerDirection.toString().toLowerCase() == 'reverse') {
-        this.setDirection = -1;
-    }
-    else {
-        throw new Error("Incorrect Controller Direction Chosen");
-    }
-};
+    this.integral = integral;
+    this.previousError = error;
+    this.lastMeasurementTime = now;
 
-/**
- * Status Functions
- * Just because you set the Kp=-1 doesn't mean it actually happened.  these
- * functions query the internal state of the PID.  they're here for display
- * purposes.  this are the functions the PID Front-end uses for example
- */
-PID.prototype.getKp = function() {
-    return this.dispKp;
-};
+    return CV;
+}
 
-PID.prototype.getKd = function() {
-    return this.dispKd;
-};
+pid.prototype.setControlValueLimits = function(lowerLimit, upperLimit, offset)
+{
+    this.CVLowerLimit = lowerLimit;
+    this.CVUpperLimit = upperLimit;
+    this.CVOffset = offset;
+}
 
-PID.prototype.getKi = function() {
-    return this.dispKi;
-};
+pid.prototype.getIntegral = function()
+{
+    return this.integral;
+}
 
-PID.prototype.getMode = function() {
-    return this.inAuto ? "Auto" : "Manual";
-};
-
-PID.prototype.getDirection = function() {
-    return this.controllerDirection;
-};
-
-PID.prototype.getOutput = function() {
-    return this.myOutput;
-};
-
-PID.prototype.getInput = function() {
-    return this.input;
-};
-
-PID.prototype.getSetPoint = function() {
-    return this.mySetpoint;
-};
-
-PID.prototype.setITerm = function(value) {
-    this.ITerm = value;
-};
-
-PID.prototype.getITerm = function() {
-    return this.ITerm;
-};
-
-PID.prototype.reset = function() {
-    this.lastInput = 0;
-    this.lastTime = this.millis() - this.SampleTime;
-    this.ITerm = 0;
-    this.myOutput = 0;
-};
-
-module.exports = PID;
+pid.prototype.setIntegral = function(integral)
+{
+    this.integral = integral;
+}
--- processes/pids.js
+++ processes/pids.js
@@ -3,11 +3,8 @@ var pid_controller = require("./lib/pid");
 
 var pids = {};
 
-function create_pid(name, description, output_component_name, cascade)
-{
-    var pid = new pid_controller(0,0,0,0,0,'direct');
-    pid.setSampleTime(1000);
-    pid.setMode("auto");
+function create_pid(name, description, output_component_name, cascade) {
+    var pid = new pid_controller();
 
     var definition = {};
 
@@ -16,13 +13,13 @@ function create_pid(name, description, output_component_name, cascade)
 
     definition.enable = cascade.create_component({
         id: name + "_pid_enable",
-        group : description,
+        group: description,
         name: description + " Enable",
         type: cascade.TYPES.BOOLEAN
     });
-    definition.enable.on("value_updated", function(){
+    definition.enable.on("value_updated", function () {
         // Reset our PID
-        if(definition.enable.value == false) {
+        if (definition.enable.value == false) {
             definition.i_term.value = null;
             definition.control_value.value = 0;
             definition.pid.reset();
@@ -31,44 +28,42 @@ function create_pid(name, description, output_component_name, cascade)
                 definition.output_component.value = 0;
             }
         }
-        else
-        {
-            definition.pid.setITerm(definition.i_term.value);
+        else {
+            definition.pid.setIntegral(definition.i_term.value);
         }
     });
 
     definition.set_point = cascade.create_component({
         id: name + "_pid_set_point",
-        name : description + " Set Point",
-        group : description,
-        read_only : false,
+        name: description + " Set Point",
+        group: description,
+        read_only: false,
         type: cascade.TYPES.NUMBER,
-        units : cascade.UNITS.F
+        units: cascade.UNITS.F
     });
 
     definition.process_value = cascade.create_component({
         id: name + "_pid_process_value",
-        name : description + " Process Value",
-        group : description,
-        read_only : true,
+        name: description + " Process Value",
+        group: description,
+        read_only: true,
         type: cascade.TYPES.NUMBER
     });
 
     definition.process_component = cascade.create_component({
         id: name + "_pid_process_component",
-        name : description + " Process Component",
-        group : description,
-        persist : true,
+        name: description + " Process Component",
+        group: description,
+        persist: true,
         type: cascade.TYPES.TEXT
     });
-    definition.process_component.on("value_updated", function(){
+    definition.process_component.on("value_updated", function () {
 
-        if(_.isUndefined(definition.process_component.value) || _.isNull(definition.process_component.value))
-        {
+        if (_.isUndefined(definition.process_component.value) || _.isNull(definition.process_component.value)) {
             definition.process_value.mirror_component(null);
         }
         else {
-            cascade.components.require_component(definition.process_component.value, function(temp_component){
+            cascade.components.require_component(definition.process_component.value, function (temp_component) {
                 definition.process_value.mirror_component(temp_component);
             });
         }
@@ -78,51 +73,51 @@ function create_pid(name, description, output_component_name, cascade)
     definition.control_value = cascade.create_component({
         id: name + "_pid_control_value",
         name: description + " Control Value",
-        group : description,
-        read_only : true,
+        group: description,
+        read_only: true,
         type: cascade.TYPES.NUMBER
     });
 
     definition.i_term = cascade.create_component({
         id: name + "_pid_i_term",
         name: description + " I Term",
-        group : description,
-        read_only : false,
+        group: description,
+        read_only: false,
         type: cascade.TYPES.NUMBER
     });
 
     definition.p_gain = cascade.create_component({
         id: name + "_pid_p_gain",
         name: description + " P Gain",
-        group : description,
+        group: description,
         class_name: "pid_gain",
-        persist : true,
+        persist: true,
         type: cascade.TYPES.NUMBER
     });
 
     definition.i_gain = cascade.create_component({
         id: name + "_pid_i_gain",
         name: description + " I Gain",
-        group : description,
+        group: description,
         class_name: "pid_gain",
-        persist : true,
+        persist: true,
         type: cascade.TYPES.NUMBER
     });
 
     definition.d_gain = cascade.create_component({
         id: name + "_pid_d_gain",
         name: description + " D Gain",
-        group : description,
+        group: description,
         class_name: "pid_gain",
-        persist : true,
+        persist: true,
         type: cascade.TYPES.NUMBER
     });
 
     definition.min_cv = cascade.create_component({
         id: name + "_pid_min_cv",
         name: description + " Minimum Control Value",
-        group : description,
-        persist : true,
+        group: description,
+        persist: true,
         type: cascade.TYPES.NUMBER,
         units: cascade.UNITS.PERCENTAGE
     });
@@ -130,21 +125,20 @@ function create_pid(name, description, output_component_name, cascade)
     definition.max_cv = cascade.create_component({
         id: name + "_pid_max_cv",
         name: description + " Maximum Control Value",
-        group : description,
-        persist : true,
+        group: description,
+        persist: true,
         type: cascade.TYPES.NUMBER,
         units: cascade.UNITS.PERCENTAGE
     });
 
-    cascade.components.require_component(output_component_name, function(output_component){
+    cascade.components.require_component(output_component_name, function (output_component) {
         definition.output_component = output_component;
     });
 
     return definition;
 }
 
-module.exports.setup = function (cascade)
-{
+module.exports.setup = function (cascade) {
     pids["pre_heater_pid"] = create_pid("pre_heater", "Preheater PID", "pre_heater_output", cascade);
     pids["main_heater_pid"] = create_pid("main_heater", "Main Heater PID", "main_heater_output", cascade);
     pids["pump"] = create_pid("pump", "Pump PID", "pump_output", cascade);
@@ -152,38 +146,29 @@ module.exports.setup = function (cascade)
     pids["tails_reflux_pid"] = create_pid("tails_reflux", "Tails Reflux PID", "tails_reflux_percent", cascade);
 };
 
-module.exports.loop = function (cascade)
-{
-    _.each(pids, function(pid_definition){
+module.exports.loop = function (cascade) {
+    _.each(pids, function (pid_definition) {
 
-        if(pid_definition.enable.value == true) {
-            pid_definition.pid.setOutputLimits(
+        if (pid_definition.enable.value == true) {
+            pid_definition.pid.setControlValueLimits(
                 pid_definition.min_cv.value || 0.0,
-                pid_definition.max_cv.value || 0.0
+                pid_definition.max_cv.value || 0.0,
+                0
             );
 
-            pid_definition.pid.setTunings(
-                pid_definition.p_gain.value || 0.0,
-                pid_definition.i_gain.value || 0.0,
-                pid_definition.d_gain.value || 0.0
-            );
+            pid_definition.pid.setProportionalGain(pid_definition.p_gain.value || 0.0);
+            pid_definition.pid.setIntegralGain(pid_definition.i_gain.value || 0.0);
+            pid_definition.pid.setDerivativeGain(pid_definition.d_gain.value || 0.0);
 
-            pid_definition.pid.setPoint(
-                pid_definition.set_point.value || 0.0
-            );
+            pid_definition.pid.setDesiredValue(pid_definition.set_point.value || 0.0);
 
-            pid_definition.pid.setInput(
-                pid_definition.process_value.value || 0.0
-            );
-
-            pid_definition.pid.compute();
-            pid_definition.control_value.value = pid_definition.pid.getOutput();
+            pid_definition.control_value.value = pid_definition.pid.update(pid_definition.process_value.value || 0.0);
 
             if (pid_definition.output_component) {
                 pid_definition.output_component.value = pid_definition.control_value.value;
             }
 
-            pid_definition.i_term.value = pid_definition.pid.getITerm();
+            pid_definition.i_term.value = pid_definition.pid.getIntegral();
         }
     });
 };
