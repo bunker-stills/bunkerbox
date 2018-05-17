f925cae1cdfa17a7d7a3aa336dadd8edc58f1ccd
Jim Heising <jheising@gmail.com>
2018-05-17 16:29:01 -0700

    Updates to load script

--- tf-redbrick-run.js
+++ tf-redbrick-run.js
@@ -1,22 +1,21 @@
 // Make sure all the NPM packages are installed
 var child_process = require('child_process');
 var path = require("path");
+var fs = require('fs');
 
 var bunkerboxDir = path.join(__dirname, "bunkerbox");
 
 var result = child_process.spawnSync("git", ["status"], {
     cwd: bunkerboxDir
 });
-if(result.status !== 0)
-{
+if (result.status !== 0) {
     var gitREPO = process.env.BUNKERBOX_CODE_REPO || "https://github.com/bunker-stills/bunkerbox.git";
     console.log("Downloading Bunkerbox code from " + gitREPO + "...");
 
     // The bunkerbox directory doesn't exist, go ahead and get latest
     result = child_process.spawnSync("git", ["clone", gitREPO]);
 
-    if(result.status !== 0)
-    {
+    if (result.status !== 0) {
         console.error("Unable to download Bunkerbox code.");
         process.exit();
         return;
@@ -30,29 +29,35 @@ result = child_process.spawnSync("git", ["pull"], {
 });
 
 // Load dependencies
-try
-{
-    var packageJSON = require(path.join(bunkerboxDir, "package.json"));
-    for(var packageName in packageJSON.dependencies)
-    {
-        var package = require(path.join(bunkerboxDir, "node_modules", packageName));
+
+var needsUpdate = false;
+var packageJSON = require(path.join(bunkerboxDir, "package.json"));
+
+for (var packageName in packageJSON.dependencies) {
+    if (!fs.existsSync(path.join(bunkerboxDir, "node_modules", packageName))) {
+        needsUpdate = true;
+        break;
     }
 }
-catch(e)
+
+if(needsUpdate)
 {
     console.log("Installing dependencies...");
     result = child_process.spawnSync("npm", ["install", "--production"], {
         cwd: bunkerboxDir
     });
 
-    if(result.status !== 0)
-    {
+    if (result.status !== 0) {
         console.error("Unable to install Bunkerbox dependencies.");
         process.exit();
         return;
     }
+}
 
-    console.log("Dependencies installed.");
+console.log("Dependencies installed.");
+
+if (!process.env.CASCADE_PROCESSES) {
+    process.env.CASCADE_PROCESSES = path.join(bunkerboxDir, "/processes/tf-rebrick");
 }
 
 // Start the bunker controller code
